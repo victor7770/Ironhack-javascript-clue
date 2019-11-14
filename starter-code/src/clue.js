3f140a91055ed73f39e7cbd0b055d0d7e34424cb
victor7770 <vvalero20363@columbushs.com>
2019-11-14 09:03:46 -0500

    first

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -113,7 +113,7 @@ const weaponsArray = [
     { name: 'rope', weight: 10 },
     { name: 'knife', weight: 8 },
     { name: 'candlestick', weight: 2 },
-    { name: 'dumbbell', weight: 30 },
+    { name: 'dumbbell', weight: 30 }, 
     { name: 'poison', weight: 2 },
     { name: 'axe', weight: 15 },
     { name: 'bat', weight: 13 },
